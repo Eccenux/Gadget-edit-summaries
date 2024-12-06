40987336ca687f97df5350fde029b89175ea921e
eccenux <eccenux+git@gmail.com>
2024-12-06 21:32:54 +0100

    linter

---
+++ .eslintrc.cjs
@@ -0,0 +1,56 @@
+module.exports = {
+	"env": {
+		"node": false,
+		"browser": true,
+		"es2017": true,
+		"mocha": false,
+	},
+	"globals": {
+		"$": true,
+		"mw": true,
+	},
+	"ignorePatterns": [
+		"dist/*",
+	],
+	"extends": "eslint:recommended",
+	"overrides": [
+		{
+			"env": {
+				"node": true,
+				"browser": true,
+			},
+			"files": [
+				".eslintrc.{js,cjs}"
+			],
+			"parserOptions": {
+				"sourceType": "script"
+			}
+		},
+		{
+			"env": {
+				"node": true,
+				"browser": false,
+				"es2021": true,
+			},
+			"files": [
+				"*.mjs",
+			],
+			"parserOptions": {
+				"sourceType": "module"
+			}
+		},
+	],
+	"rules": {
+		"no-prototype-builtins": "off",
+		"indent": [
+			"error",
+			"tab",
+			{
+				"SwitchCase": 1,
+			},
+		],
+		//"array-bracket-newline": ["error", { "multiline": true, "minItems": 3 }],
+		//"array-element-newline": ["error", { "multiline": true }]
+		"array-element-newline": ["error", "consistent"]
+	}
+}
