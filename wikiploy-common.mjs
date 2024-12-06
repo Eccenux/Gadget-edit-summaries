591b6f1d0378ea3e998b1d2adb58b79950433bc4
eccenux <eccenux+git@gmail.com>
2024-12-06 21:43:31 +0100

    wikiploy basic

---
+++ wikiploy-common.mjs
@@ -0,0 +1,24 @@
+import { DeployConfig } from 'wikiploy';
+
+/**
+ * Add config.
+ * @param {Array} configs DeployConfig array.
+ * @param {String} site Domian of a MW site.
+ */
+export function addConfig(configs, site, isRelease = false) {
+	let deploymentName = isRelease ? 'MediaWiki:Gadget-edit-summaries-' : '~/edit-summaries-';
+	configs.push(new DeployConfig({
+		src: 'core.js',
+		dst: `${deploymentName}core.js`,
+		site,
+		nowiki: true,
+	}));
+	configs.push(new DeployConfig({
+		src: 'local.js',
+		dst: `${deploymentName}local.js`,
+		site,
+	}));
+}
+export function addConfigRelease(configs, site) {
+	addConfig(configs, site, true);
+}
---
+++ wikiploy.mjs
@@ -0,0 +1,26 @@
+import { Wikiploy, setupSummary } from 'wikiploy';
+
+import * as botpass from './bot.config.mjs';
+const ployBot = new Wikiploy(botpass);
+
+// common deploy function(s)
+import { addConfig, addConfigRelease } from './wikiploy-common.mjs';
+
+// run asynchronously to be able to wait for results
+(async () => {
+	// custom summary from a prompt
+	await setupSummary(ployBot);
+
+	// push out file(s) to wiki
+	const configs = [];
+	// dev version
+	addConfig(configs, 'pl.wikipedia.org');
+	// release versions
+	addConfigRelease(configs, 'pl.wikipedia.org');
+
+	await ployBot.deploy(configs);
+
+})().catch(err => {
+	console.error(err);
+	process.exit(1);
+});
